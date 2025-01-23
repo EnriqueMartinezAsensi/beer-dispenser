b55bc949ab3965d8d1a69009b1ae3c1d80b37715
EnriqueMartinezAsensi <emartinezasensi@gmail.com>
2025-01-23 09:21:15 +0100

    first MUI components added

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,52 @@
 import { RouteObject } from "react-router-dom";
-import Dispensers from "../pages/dispensers";
+
+//-----Free accsess elements-----
+import PageWrapper from "../components/PageWrapper";
+import ErrorPage from "../pages/ErrorPage";
+import Dispensers from "../pages/Dispensers";
+import DispenserScreen from "../pages/DispenserScreen";
+import LogInPage from "../pages/LogInPage";
+import ProtectedPage from "../pages/ProtectedPage";
+
+//------Restricted access elements-----
+import AdminPanel from "../pages/AdminPanel";
+import DispenserStats from "../pages/DispenserStats";
 
 const routes: RouteObject[] = [
   {
     path: "/",
-    element: <Dispensers />,
+    element: <PageWrapper />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Dispensers />,
+      },
+      {
+        path: "/:id",
+        element: <DispenserScreen />,
+      },
+      {
+        path: "/login",
+        element: <LogInPage />,
+      },
+      {
+        path: "/admin",
+        element: (
+          <ProtectedPage>
+            <AdminPanel />
+          </ProtectedPage>
+        ),
+      },
+      {
+        path: "/admin/:id",
+        element: (
+          <ProtectedPage>
+            <DispenserStats />
+          </ProtectedPage>
+        ),
+      },
+    ],
   },
 ];
 
